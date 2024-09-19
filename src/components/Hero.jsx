502dcc1ee886bcb5e880417aa9465f74683896dd
Abiye <abiyerowland@gmail.com>
2024-09-19 16:27:51 +0100

    normal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import HeroImage from '../img/HeroImage.jpeg'
 import { Link } from 'react-router-dom';
 function Hero() {
   return (
-    <div className="h-screen bg-slate-400 text-black flex flex-col justify-center items-center px-4 sm:px-8">
+    <div className="h-screen text-black flex flex-col justify-center items-center px-4 sm:px-8">
       <img src={HeroImage}alt="Hero Image" className="w-[400px] border-2 rounded object-cover" />
       <h1 className="text-5xl font-bold mt-4">Elevate Your Everyday</h1>
       <p className="text-lg mt-4">Discover timeless pieces for every occasion.</p>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,34 +1,40 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom';
-import { FaCartPlus } from "react-icons/fa"
-import { FaInfoCircle } from "react-icons/fa"
-import { CartContext } from '../contexts/CartContext';
-
+import React, {useContext} from "react";
+import {Link} from "react-router-dom";
+import {FaCartPlus} from "react-icons/fa";
+import {CartContext} from "../contexts/CartContext";
 
 const Products = ({product}) => {
-  const {id, image, title, price} = product
-  const {addtoCart} = useContext(CartContext)
-  
-  return (
-    <div>
-    <Link key={id} to={`/product/${id}`}>
-     <div className='border-2 border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition '>
-      <div className='w-full h-full flex justify-center items-center'>
-        <div className='w-[200px] mx-auto flex justify-center items-center'>
-          <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={image} alt="" />
-        </div>
-      </div>
-     </div>
-     <div>
-      <Link to={`/product/${id}`} ><strong>{title}</strong></Link>
-      <div className='flex  items-center p-2 text-xl justify-around'>
-        <h1 className='text-xl'>₦ {price}</h1>
-        <button className='bg-green-500 p-2 rounded-lg' onClick={()=> addtoCart(product,id)} ><FaCartPlus/></button>
-      </div>
-     </div>
-    </Link>
-    </div>
-  )
-}
+    const {id, image, title, price} = product;
+    const {addtoCart} = useContext(CartContext);
 
-export default Products
+    return (
+        <div>
+            <Link key={id} to={`/product/${id}`}>
+                <div
+                    className="border-2 border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition ">
+                    <div className="w-full h-full flex justify-center items-center">
+                        <div className="w-[200px] mx-auto flex justify-center items-center">
+                            <img
+                                className="max-h-[160px] group-hover:scale-110 transition duration-300"
+                                src={image}
+                                alt=""/>
+                        </div>
+                    </div>
+                </div>
+                <button
+                    className="bg-green-500 flex justify-center w-full p-2 rounded-lg"
+                    onClick={() => addtoCart(product, id)}>
+                    <FaCartPlus/>
+                </button>
+                <div>
+                    <Link to={`/product/${id}`}>
+                        <strong>{title}</strong>
+                    </Link>
+                    <h1 className="text-xl">₦ {price}</h1>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default Products;
